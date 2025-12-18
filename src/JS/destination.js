a53724d6436b151a0157cf5e49e356c327162b9b
sakshi13025 <sakshi13025@gmail.com>
2025-12-18 10:15:29 +0530

    Day2 Done

---
+++ src/JS/destination.js
@@ -0,0 +1,11 @@
+const params = new URLSearchParams(window.location.search);
+
+const city = params.get("city");
+
+const cityName = document.getElementById("cityName");
+
+if (city) {
+    cityName.textContent = city;
+} else {
+cityName.textContent = "Unknown City";
+}
