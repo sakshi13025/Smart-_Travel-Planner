4b3af4588cb5976865bc8b19f3ae8f2e7c850d1d
sakshi13025 <sakshi13025@gmail.com>
2025-12-24 10:14:59 +0530

    UI Part Done

--- src/JS/weather.js
+++ src/JS/weather.js
@@ -8,21 +8,31 @@ const errorBox = document.querySelector("#error");
 
 let API_KEY = "dfad442bbdf67ea99a2952317ae4e1d0";
 
-if(!city){
-loading.classList.add("hidden");
-errorBox.classList.remove("hidden");
+if (!city) {
+  loading.classList.add("hidden");
+  errorBox.classList.remove("hidden");
 } else {
-cityName.textContent = city;
+  cityName.textContent = city;
 
-async function fetchWeather(cityName) {
+  async function fetchWeather(cityName) {
     try {
+      const geoRes = await fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`
+      );
+      console.log(geoRes);
 
+      if (!geoRes.ok) throw new Error("Geo API failed");
+
+      const geoData = await geoRes.json();
+      console.log(geoData);
+
+      let { lat, lon } = geoData[0];
+      console.log(lat, lon);
     } catch (error) {
-        console.log(error, "Error in Fetching Weather Details");
-        loading.classList.add("hidden");
-        errorBox.classList.remove("hidden");
-        
+      console.log(error, "Error in Fetching Weather Details");
+      loading.classList.add("hidden");
+      errorBox.classList.remove("hidden");
     }
-}
-fetchWeather(city)
+  }
+  fetchWeather(city);
 }
